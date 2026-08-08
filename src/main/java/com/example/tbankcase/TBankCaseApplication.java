b805c89d1c9d1979db0b78050b4e5bf75039b3d4
Сидельников Алексей Александрович <it2514saa6@susu.ru>
2026-08-08 14:22:23 +0500

    Initial commit

---
+++ src/main/java/com/example/tbankcase/TBankCaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.tbankcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TBankCaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TBankCaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tbankcase/TBankCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tbankcase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TBankCaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
